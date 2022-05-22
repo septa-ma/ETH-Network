44018f592a1cd133f47aeef8d0ae4e4a94394f00
root <maryamasadi586@gmail.com>
2022-05-23 00:39:17 +0430

    step 2

---
+++ checkingTRX.js
@@ -0,0 +1,35 @@
+// require the web3 package
+const Web3 = require('web3');
+
+class checkingTRX {
+    web3;
+    account;
+
+    // connect to the ETH network
+    constructor(projectId, account) {
+        this.web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/' + projectId));
+        this.account = account.toLowerCase();
+    }
+
+    // checking the block
+    async checkBlock(trxHash) {
+        let block = await this.web3.eth.getBlock(trxHash);
+        let number = block.number;
+        console.log('Finding a block ' + number);
+        
+        // if the block was found and had trxs
+        if (block != null && block.transactions != null) {
+
+            // loop trough block's trxs
+            for (let txHash of block.transactions) { // check trxhash here!!
+                let tx = await this.web3.eth.getTransaction(txHash);
+                console.log('Transaction found on block: ' + number);
+                console.log({address: tx.from, value: this.web3.utils.fromWei(tx.value, 'ether'), timestamp: new Date()});
+            } // end for
+            
+        } // end if
+    } // end function
+
+} // end class
+
+module.exports.checkingTRX = checkingTRX;
---
+++ main.js
@@ -0,0 +1,8 @@
+const checkingTRX = require('./checkingTRX');
+
+var project_id = '86e44087cd3a4cd4a130112fca17586c';
+var account_id = '312ed35460ce4b78baba08f374904280';
+var trxHash = 'latest';
+
+let trxChecker = new checkingTRX.checkingTRX(project_id, account_id);
+trxChecker.checkBlock(trxHash);
