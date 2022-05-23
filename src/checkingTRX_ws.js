16e16bfbcd8ea47f9ae1a4b6c72fb3e5b55a74fa
root <maryamasadi586@gmail.com>
2022-05-23 15:39:58 +0430

    step 4

--- src/checkingTRX_ws.js
+++ src/checkingTRX_ws.js
@@ -3,38 +3,26 @@ const Web3 = require('web3');
 
 class checkingTRXWS {
     web3;
-    web3ws;
     account;
-    subscription;
 
-    // for haveing better connection use WebSocketProvider
     constructor(projectId, account) {
-        this.web3ws = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/' + projectId));
         this.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + projectId));
         this.account = account.toLowerCase();
     }
 
-    subscribe(topic) {
-        this.subscription = this.web3ws.eth.subscribe(topic, (err, res) => {
-            if (err) console.error(err);
-        });
-    }
-
-    watchTransactions() {
-        console.log('Watching all pending transactions...');
-        this.subscription.on('data', (txHash) => {
-            setTimeout(async () => {
-                try {
-                    let tx = await this.web3.eth.getTransaction(txHash);
-                    if (tx != null) {
-                        console.log({address: tx.from, value: this.web3.utils.fromWei(tx.value, 'ether'), timestamp: new Date()});
-                    }
-                } catch (err) {
-                    console.error(err);
-                }
-            }, 60000)
-        });
-    } // end function 
+    async getConfirmations(txHash) {
+        try {
+            // get transaction details
+            let trx = await this.web3.eth.getTransaction(txHash);
+            if (trx == null) {
+                console.log('there is not a confirmation with this txHash.');
+            } else {
+                console.log({address: trx.from, value: this.web3.utils.fromWei(trx.value, 'ether'), timestamp: new Date()});
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    } // end function
 
 } // end class
 
--- src/main_ws.js
+++ src/main_ws.js
@@ -2,7 +2,7 @@ const checkingTRXWS = require('./checkingTRX_ws');
 
 var project_id = '86e44087cd3a4cd4a130112fca17586c';
 var account_id = '312ed35460ce4b78baba08f374904280';
+var trxHash = '0x528f8ddd36f15b0855ca0fd6aadb64344e01fc87716e2a5283852020c17b425d';
 
 let trxCheckerWS = new checkingTRXWS.checkingTRXWS(project_id, account_id);
-trxCheckerWS.subscribe('pendingTransactions');
-trxCheckerWS.watchTransactions();
+trxCheckerWS.getConfirmations(trxHash);
